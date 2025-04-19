08e297a75aef44c217d83c04e5bcabfa115fab54
aiden-d <aidendawes@gmail.com>
2025-04-19 12:10:52 +0100

    load data bug fix

--- models/model_B/run_experiments.py
+++ models/model_B/run_experiments.py
@@ -4,5 +4,5 @@ After running grid search:
 2. create xperiment files with the best parameters
 3. run the full_pipeline.py to train again + save the model + run test set + save the plots -> supercomputer
 (make sure all plots we need are integrated)
-4. choose the best experiments from results folder 
+4. choose the best experiments from results folder
 """
